52c128f255ab58e0a42b3a797f71ddb1ce0c8edd
2Ruk <94098919+2Ruk@users.noreply.github.com>
2023-04-02 19:13:18 +0900

    feat: add font

--- cheer-up/src/components/quote.js
+++ cheer-up/src/components/quote.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import TimerIcon from '@mui/icons-material/Timer';
 
 const Quote = ({ quote }) => (
   <div >
-    <h3>같이, 힘내요 </h3>
-    <p className="fade-in-box" key={quote}>{quote}</p>
+    <h3>같이, 힘내요      <TimerIcon /> </h3>
+    <p className="fade-in-box shadow" key={quote}>{quote}</p>
     <h3>잠시, 쉬어가요</h3>
   </div>
 );
