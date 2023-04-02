1fb953ac0aab948fb7b372faad0f6dd1207c8121
2Ruk <94098919+2Ruk@users.noreply.github.com>
2023-04-02 17:02:35 +0900

    feat: add util

---
+++ cheer-up/src/util/recomendYoutube.js
@@ -0,0 +1,22 @@
+export const recommendYoutubeList = [
+    {
+      title: '상처 많은 널 위로해줄 노래',
+      youtuber: '뮤마Muma',
+      videoId: 'nKbBTy7ioxI'
+    },
+    {
+        title: '소리 내어 울지 못하는 그대에게',
+        youtuber: '뮤마Muma',
+        videoId: 'uMocVxNe6OU'
+      },
+      {
+        title: '우리는 어째서 어른이 된 걸까',
+        youtuber: 'MUTE',
+        videoId: 'UqGn7T0Qysk'
+      },{
+        title: '위로받고 싶을 때 듣는 밍기뉴 플레이리스트',
+        youtuber: '세제원샷',
+        videoId: 'lwMEKstov4o'
+      }
+  ];
+  
